7dfc039934bc3ac3f40b78a1576e5321c600aac7
badkicker <ram2rakhi@gmail.com>
2018-10-05 01:24:01 +0530

    sa

--- src/server.js
+++ src/server.js
@@ -5,14 +5,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(favicon(__dirname + '/dist/favicon.ico'));
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
