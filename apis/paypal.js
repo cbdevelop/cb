67b0d90209f0284fba2fb81a12c962a299133151
nndsrinivas123@gmail.com <nndsrinivas123@gmail.com>
2018-10-13 23:50:20 +0530

    paypal chngs

--- apis/paypal.js
+++ apis/paypal.js
@@ -37,8 +37,8 @@ module.exports = () => {
                 "payment_method": "paypal"
             },
             "redirect_urls": {
-                "return_url": app.locals.baseurl + "/success",
-                "cancel_url": app.locals.baseurl + "/cancel"
+                "return_url": app.locals.baseurl + "payment/success",
+                "cancel_url": app.locals.baseurl + "payment/cancel"
             },
             "transactions": [{
                 "amount": {
@@ -63,19 +63,6 @@ module.exports = () => {
                             redirectUrl = link.href;
                         }
                     }
-                    // Website you wish to allow to connect
-                    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-                    // Request methods you wish to allow
-                    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-                    // Request headers you wish to allow
-                    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-                    // Set to true if you need the website to include cookies in the requests sent
-                    // to the API (e.g. in case you use sessions)
-                    res.setHeader('Access-Control-Allow-Credentials', true);
-
                     res.redirect(redirectUrl);
                 }
             }
