edc16b4c9eef05f5a3f8274bbc4866ba7a8a776a
badkicker <ram2rakhi@gmail.com>
2018-10-26 00:44:23 +0530

    loading

--- src/app/checkout/event-manager/addMore/addMore.component.ts
+++ src/app/checkout/event-manager/addMore/addMore.component.ts
@@ -49,22 +49,32 @@ export class AddMoreComponent implements OnInit {
     if (this.masterObj.searchObj.serviceType[0].id == 1) {
       this.masterObj.serviceCost += 600;  // drop off
     } else if (this.masterObj.searchObj.serviceType[0].id == 2) {
-
+      let starterArr = this.masterObj.selectedDishArr.filter(x => x.Menu_Type == 1);
+      let snackArr = this.masterObj.selectedDishArr.filter(x => x.Menu_Type == 2);
+      let saladArr = this.masterObj.selectedDishArr.filter(x => x.Menu_Type == 6);
       let mainArr = this.masterObj.selectedDishArr.filter(x => x.Menu_Type == 7);
       let breadArr = this.masterObj.selectedDishArr.filter(x => x.Menu_Type == 4);
       let dessertArr = this.masterObj.selectedDishArr.filter(x => x.Menu_Type == 8);
-      this.masterObj.serviceCost += (mainArr.length * 800) + (breadArr.length * 400) + (dessertArr.length * 1000);
-
+      if(this.masterObj.totalAttendees <= 500)
+        this.masterObj.serviceCost +=(starterArr.length * 800) + (snackArr.length * 800) + (saladArr.length * 800) + (mainArr.length * 800) + (breadArr.length * 400) + (dessertArr.length * 1000);
+      else
+        this.masterObj.serviceCost += 2*((starterArr.length * 800) + (snackArr.length * 800) + (saladArr.length * 800) +(mainArr.length * 800) + (breadArr.length * 400) + (dessertArr.length * 1000));
     } else if (this.masterObj.searchObj.serviceType[0].id == 3) {
       this.masterObj.serviceCost += (this.masterObj.totalAttendees * 50);
 
     } else if (this.masterObj.searchObj.serviceType[0].id == 4) {
 
+      let starterArr = this.masterObj.selectedDishArr.filter(x => x.Menu_Type == 1);
+      let snackArr = this.masterObj.selectedDishArr.filter(x => x.Menu_Type == 2);
+      let saladArr = this.masterObj.selectedDishArr.filter(x => x.Menu_Type == 6);
       let mainArr = this.masterObj.selectedDishArr.filter(x => x.Menu_Type == 7);
       let breadArr = this.masterObj.selectedDishArr.filter(x => x.Menu_Type == 4);
       let dessertArr = this.masterObj.selectedDishArr.filter(x => x.Menu_Type == 8);
       this.masterObj.serviceCost += (this.masterObj.totalAttendees * 50);
-      this.masterObj.serviceCost += (mainArr.length * 800) + (breadArr.length * 400) + (dessertArr.length * 1000);
+      if(this.masterObj.totalAttendees <= 500)
+        this.masterObj.serviceCost += (starterArr.length * 800) + (snackArr.length * 800) + (saladArr.length * 800) + (mainArr.length * 800) + (breadArr.length * 400) + (dessertArr.length * 1000);
+      else
+        this.masterObj.serviceCost += 2*((starterArr.length * 800) + (snackArr.length * 800) + (saladArr.length * 800) +(mainArr.length * 800) + (breadArr.length * 400) + (dessertArr.length * 1000));
     } else {
 
     }
